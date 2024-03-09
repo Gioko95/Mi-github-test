079e4ee55f9d40ca8d52b5799d02486cbba1ebc4
Gioko95 <162385269+Gioko95@users.noreply.github.com>
2024-03-08 19:13:15 -0500

    Actualizacion

---
+++ javascript.js
@@ -0,0 +1,11 @@
+"use strict"
+// Hacer un programa que tenga un saludo
+/* Console.log(para visualizar)
+document.write("hola")*/
+
+const ingresesunombre prompt=("ingrese su nombre")
+ switch (ingresesunombre) {
+    case "";
+     document.write("Hola desconocido");
+    break;
+ }
