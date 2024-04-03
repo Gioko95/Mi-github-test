ce64cada85b2dac7ca680d1276ea4b1f5bebda4e
Gioko95 <162385269+Gioko95@users.noreply.github.com>
2024-04-03 18:32:31 -0500

    Actualizacion de todo el repo

--- Proyectos/Pruebas.js
+++
@@ -1,9 +0,0 @@
-"use stric"
-// Ingresar un nombre y apellido para que de como resultado el nombre completo
-
-const nombre1 = prompt("ingrese su primer nombre");
-const nombre2 = prompt("ingrese su segundo nombre");
-const apellido1 = prompt("ingrese su primer apellido");
-const apellido2 = prompt("ingrese su segundo apellido");
-document.write(`Su nombre completo es ${nombre1} ${nombre2} ${apellido1} ${apellido2}`);
-
